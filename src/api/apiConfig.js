01a94b51514667783d52caae5894dd6e922813d8
Durgesh Sahu <72385938+durgeshsahu10@users.noreply.github.com>
2025-10-16 14:31:09 +0530

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'ed0e084847b7a9a8fcccba964a4c8faf',
+    apiKey: '########  ENTER YOUR OWN API KEY ######',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
